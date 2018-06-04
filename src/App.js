7744ea2eed53df27c5e30c21fc5a16ce8212fa32
Falon Darville <falondarville@gmail.com>
2018-06-04 14:29:19 -0700

    start styling

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav class="navbar navbar-light bg-light">
+          <span class="navbar-brand mb-0 h1">Memory Game</span>
+        </nav>
+        <div className="jumbotron">
+          <p>Click on an image to begin. The goal of the game is to only click on images that you have not previously clicked on during that game. Don't click the same photo twice!</p>
+          <span>Current Score: </span>
+          <br />
+          <span>Top Score: </span>
+        </div>
       </div>
     );
   }

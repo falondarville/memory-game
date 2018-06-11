66cbab2e89915ee528385f45e20298f97c59538b
Falon Darville <falondarville@gmail.com>
2018-06-11 15:14:57 -0700

    start authors objects and map render

--- src/App.js
+++ src/App.js
@@ -1,6 +1,66 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const authors = [
+  {
+    id: 1,
+    url: window.location.origin + "/images/murakami.png",
+    alt: "Murakami"
+    clicked: false
+  },
+  {
+    id: 2,
+    url: window.location.origin + "/images/austen.png",
+    alt: "Austen",
+    clicked: false
+  },
+  {
+    id: 3,
+    url: window.location.origin + "/images/wells.png",
+    alt: "Wells",
+    clicked: false
+  },
+  {
+    id: 4, 
+    url: window.location.origin + "/images/stevenson.png",
+    alt: "Stevenson",
+    clicked: false
+  },
+  {
+    id: 5,
+    url: window.location.origin + "/images/gilman.png",
+    alt: "Gilman", 
+    clicked: false
+  },
+  {
+    id: 6,
+    url: window.location.origin + "/images/orwell.png",
+    alt: "Orwell", 
+    clicked: false
+  },
+  {
+    id: 7,
+    url: window.location.origin + "/images/hesse.png",
+    alt: "Hesse",
+    clicked: false
+  },
+  {
+    id: 8
+  },
+  {
+    id: 9
+  },
+  {
+    id: 10
+  },
+  {
+    id: 11
+  },
+  {
+    id: 12
+  }
+]
+
 class App extends Component {
   render() {
     return (
--- src/Components/authors.js
+++ src/Components/authors.js
@@ -5,9 +5,11 @@ export default class Authors extends Component {
 	render() {
     return (
     	// cycle through images and display them in random order upon click of any image. Images should have unique ids to keep track whether or not the image has been previously clicked. 
-    	<div className="col-sm">
-            <img className="authors" src={window.location.origin + "/images/murakami.png"} alt="Murakami"/>
-        </div>
+    	{props.authors.map(author => (
+	    	<div className="col-sm">
+	            <img className="authors" src={author.url} alt={author.alt} key={author.id}/>
+	        </div>
+    		))}
        );
   }
 }

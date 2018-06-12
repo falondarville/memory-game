fc67245b8d5b521bf570a7094437e07db6e95399
Falon Darville <falondarville@gmail.com>
2018-06-12 12:53:10 -0700

    make authors into class and add state

--- src/App.js
+++ src/App.js
@@ -2,82 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Authors from './Components/authors.js';
 
-// authors rendered to page
-const authors = [
-  {
-    id: 1,
-    url: window.location.origin + "/images/murakami.png",
-    alt: "Murakami",
-    clicked: false
-  },
-  {
-    id: 2,
-    url: window.location.origin + "/images/austen.png",
-    alt: "Austen",
-    clicked: false
-  },
-  {
-    id: 3,
-    url: window.location.origin + "/images/wells.png",
-    alt: "Wells",
-    clicked: false
-  },
-  {
-    id: 4, 
-    url: window.location.origin + "/images/stevenson.png",
-    alt: "Stevenson",
-    clicked: false
-  },
-  {
-    id: 5,
-    url: window.location.origin + "/images/gilman.png",
-    alt: "Gilman", 
-    clicked: false
-  },
-  {
-    id: 6,
-    url: window.location.origin + "/images/orwell.png",
-    alt: "Orwell", 
-    clicked: false
-  },
-  {
-    id: 7,
-    url: window.location.origin + "/images/hesse.png",
-    alt: "Hesse",
-    clicked: false
-  },
-  {
-    id: 8,
-    url: window.location.origin + "/images/wilde.png",
-    alt: "Wilde",
-    clicked: false
-  },
-  {
-    id: 9,
-    url: window.location.origin + "/images/joyce.png",
-    alt: "Joyce",
-    clicked: false
-  },
-  {
-    id: 10,
-    url: window.location.origin + "/images/marx.png",
-    alt: "Marx",
-    clicked: false
-  },
-  {
-    id: 11,
-    url: window.location.origin + "/images/angelou.png",
-    alt: "Angelou",
-    clicked: false
-  },
-  {
-    id: 12,
-    url: window.location.origin + "/images/frank.png",
-    alt: "Frank",
-    clicked: false
-  }
-];
-
 let clickStreak = 0;
 let topScore = 0;
 
@@ -85,8 +9,7 @@ export default class App extends Component {
 
   state = {
     clickStreak,
-    topScore,
-    authors
+    topScore
   }
 
   render(){
@@ -110,7 +33,7 @@ export default class App extends Component {
         </div>
         <div className="container">
             {/* author images will render here */}
-            <Authors authors={this.state.authors}/>
+            <Authors />
         </div>
       </div>
       )
--- src/Components/authors.js
+++ src/Components/authors.js
@@ -1,27 +1,115 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// cycle through images and display them in random order upon click of any image. 
-const handleClicked = (event) => {
-	// change Clicked on the item from false to true. If Clicked is already true, then the game should reset: points to 0, images shuffle, and Clicked on all items is changed to false. 
-	alert("this was clicked");
-	function shuffle(authors) {
-	for(let i = authors.length -1; i > 0; i--){
-		const j = Math.floor(Math.random() * (i + 1));
-        [authors[i], authors[j]] = [authors[j], authors[i]];
+// authors rendered to page
+const authors = [
+  {
+    id: 1,
+    url: window.location.origin + "/images/murakami.png",
+    alt: "Murakami",
+    clicked: false
+  },
+  {
+    id: 2,
+    url: window.location.origin + "/images/austen.png",
+    alt: "Austen",
+    clicked: false
+  },
+  {
+    id: 3,
+    url: window.location.origin + "/images/wells.png",
+    alt: "Wells",
+    clicked: false
+  },
+  {
+    id: 4, 
+    url: window.location.origin + "/images/stevenson.png",
+    alt: "Stevenson",
+    clicked: false
+  },
+  {
+    id: 5,
+    url: window.location.origin + "/images/gilman.png",
+    alt: "Gilman", 
+    clicked: false
+  },
+  {
+    id: 6,
+    url: window.location.origin + "/images/orwell.png",
+    alt: "Orwell", 
+    clicked: false
+  },
+  {
+    id: 7,
+    url: window.location.origin + "/images/hesse.png",
+    alt: "Hesse",
+    clicked: false
+  },
+  {
+    id: 8,
+    url: window.location.origin + "/images/wilde.png",
+    alt: "Wilde",
+    clicked: false
+  },
+  {
+    id: 9,
+    url: window.location.origin + "/images/joyce.png",
+    alt: "Joyce",
+    clicked: false
+  },
+  {
+    id: 10,
+    url: window.location.origin + "/images/marx.png",
+    alt: "Marx",
+    clicked: false
+  },
+  {
+    id: 11,
+    url: window.location.origin + "/images/angelou.png",
+    alt: "Angelou",
+    clicked: false
+  },
+  {
+    id: 12,
+    url: window.location.origin + "/images/frank.png",
+    alt: "Frank",
+    clicked: false
+  }
+];
+
+export default class Authors extends Component {
+
+	shuffle(authors) {
+		for(let i = authors.length -1; i > 0; i--){
+			const j = Math.floor(Math.random() * (i + 1));
+	        [authors[i], authors[j]] = [authors[j], authors[i]];
+	        return authors;
 		}
-	shuffle(authors);
+	}
+	// cycle through images and display them in random order upon click of any image. 
+	handleClicked = (event) => {
+	// change Clicked on the item from false to true. If Clicked is already true, then the game should reset: points to 0, images shuffle, and Clicked on all items is changed to false. 
+		let authors = this.shuffle(this.state.authors);
+		this.setState({authors});
+
+		console.log("this was clicked");
+	}
+
+	constructor(){
+		super();
+		this.state = {
+			authors
+		}
+	}
+
+	render(){
+		return (
+			<div className="row">
+				{this.state.authors.map(author => (
+			    	<div className="col-sm-3" key={author.id} onClick={this.handleClicked}>
+			            <img className="authors" src={author.url} alt={author.alt}/>
+			        </div>
+			    ))}
+		    </div>
+		)
 	}
 }
-
-// use shuffle function and map through shuffled authors array
-const Authors = props => (
-	<div className="row">
-		{props.authors.map(author => (
-	    	<div className="col-sm-3" key={author.id} onClick={handleClicked}>
-	            <img className="authors" src={author.url} alt={author.alt}/>
-	        </div>
-	    ))}
-    </div>
-);
-
-export default Authors;
